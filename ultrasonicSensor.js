9bf000c32a7b10bb5bebd90a0f7da7468b4cb78f
Emre Akcadag <akcadagemre@gmail.com>
2021-05-02 22:45:13 +0300

    ultrasonicSensor

--- ultrasonicSensor.js
+++ ultrasonicSensor.js
@@ -29,7 +29,7 @@ const watchHCSR04 = () => {
             }
 
             if (delay < 10) {
-                delay = 2
+                buzzer.digitalWrite(1)
             }
 
             setBuzzer(delay)
