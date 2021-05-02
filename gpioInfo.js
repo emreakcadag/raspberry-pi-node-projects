97a0dd6e45674f20e9bdc070bb0916f31710ae91
Emre Akcadag <akcadagemre@gmail.com>
2021-05-02 16:22:33 +0300

    require fix

--- gpioInfo.js
+++ gpioInfo.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const Gpio = require('pigpio').Gpio;
+const Gpio = require('bindings')('pigpio.node');
 
 for (let gpioNo = Gpio.MIN_GPIO; gpioNo <= Gpio.MAX_GPIO; gpioNo += 1) {
   const gpio = new Gpio(gpioNo);
