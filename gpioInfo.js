68565f87fb2ffc90f9c4cb0fb249b23910afddab
Emre Akcadag <akcadagemre@gmail.com>
2021-05-02 15:29:19 +0300

    Initial commit

---
+++ gpioInfo.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const Gpio = require('pigpio').Gpio;
+
+for (let gpioNo = Gpio.MIN_GPIO; gpioNo <= Gpio.MAX_GPIO; gpioNo += 1) {
+  const gpio = new Gpio(gpioNo);
+
+  console.log('GPIO ' + gpioNo + ':' +
+    ' mode=' + gpio.getMode() +
+    ' level=' + gpio.digitalRead()
+  );
+}
