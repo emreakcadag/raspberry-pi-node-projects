3601b65c573d390cca72e16c62c0d9c56ae31f9e
Emre Akcadag <akcadagemre@gmail.com>
2021-05-02 21:56:54 +0300

    ultrasonicSensor

--- ultrasonicSensor.js
+++ ultrasonicSensor.js
@@ -5,17 +5,20 @@ const MICROSECDONDS_PER_CM = 1e6 / 34321;
 
 const trigger = new Gpio(23, {mode: Gpio.OUTPUT});
 const echo = new Gpio(24, {mode: Gpio.INPUT, alert: true});
+const buzzer = new Gpio(25, {mode: Gpio.OUTPUT, alert: true});
 
 trigger.digitalWrite(0); // Make sure trigger is low
+buzzer.digitalWrite(0); // Make sure buzzer is low
 
 const watchHCSR04 = () => {
     let startTick;
 
     echo.on('alert', (level, tick) => {
-        if (level === 1) {
+        if (level == 1) {
             startTick = tick;
         } else {
-            const diff = (tick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+            const endTick = tick;
+            const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
             console.log(diff / 2 / MICROSECDONDS_PER_CM);
         }
     });
